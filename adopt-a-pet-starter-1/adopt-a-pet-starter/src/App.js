ffb1ee4b34fc483ce6d17a8739a46e9a4f1ea78f
sfia-o <sofia.vl.oliveira@gmail.com>
2024-01-26 10:22:00 -0800

    Add route to pet details page.

--- adopt-a-pet-starter-1/adopt-a-pet-starter/src/App.js
+++ adopt-a-pet-starter-1/adopt-a-pet-starter/src/App.js
@@ -10,6 +10,9 @@ function App() {
     <div>
       <Router>
         <Navigation />
+        <Route path="/:type/:id">
+          <PetDetailsPage />
+        </Route>
         <Route path="/:type?">
           <HomePage />
         </Route>
